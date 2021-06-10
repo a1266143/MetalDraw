3dcd69aa9e30c8fb5732369a8f52dd3b9c2fae4e
xiaojun <603820467@qq.com>
2021-06-10 22:12:01 +0800

    使用Metal通过MTKView绘制背景色完成

--- MetalDrawContent/ViewController.swift
+++ MetalDrawContent/ViewController.swift
@@ -21,7 +21,7 @@ class ViewController: UIViewController {
     private func doInit(){
         mtkView.device = MTLCreateSystemDefaultDevice()
         //将MTKView的背景抹去为背景色
-        mtkView.clearColor = MTLClearColorMake(0.0, 0.5, 1.0, 1.0)
+        mtkView.clearColor = MTLClearColorMake(1.0, 1.0, 0.0, 1.0)
         //将View设置成在需要时才刷新
         mtkView.enableSetNeedsDisplay = true
         mtkView.delegate = self
@@ -44,8 +44,19 @@ extension ViewController:MTKViewDelegate{
         if renderPassDes == nil {
             return
         }
+        ///创建CommandQueue
+        let commandQueue = view.device?.makeCommandQueue()
+        let commandBuffer = commandQueue?.makeCommandBuffer()
         //创建Render Pass<渲染通道>
-        
+        let commandEncoder = commandBuffer?.makeRenderCommandEncoder(descriptor: renderPassDes!)
+        commandEncoder?.endEncoding()
+        //注意，并不是所有的纹理<textures>都会显示在屏幕上(当前个人理解:有些纹理可能是用来计算)
+        //为了显示在屏幕上，在Metal中，是使用Drawable对象来实现的
+        let drawable = view.currentDrawable
+        //present方法告诉Metal,当commandBuffer被执行的时候，当渲染完成时，就显示出来
+        commandBuffer?.present(drawable!)
+        //由于你执行了所有的操作，现在可以提交commandBuffer了
+        commandBuffer?.commit()
         
     }
     
